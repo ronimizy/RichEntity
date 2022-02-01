e87104dc01a6a67db4312e1a9ec5711f00af693f
ronimizy <71693183+ronimizy@users.noreply.github.com>
2022-02-01 22:48:10 +0300

    refactor: added test cases to .Sample

--- Analyzers/RichEntity.Analyzers.Sample/Invalid.cs
+++ Analyzers/RichEntity.Analyzers.Sample/Invalid.cs
@@ -34,25 +34,43 @@ public class Invalid
 
     public class Context : DbContext
     {
+        private const string Children = "_childrens";
+        private const string Name = "name";
+        private const string Father = "father";
+        private const string Friends = "_friends";
+
         public DbSet<Person> Persons { get; private set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Person>().Navigation(p => p.Children).HasField("_childrens");
+            modelBuilder.Entity<Person>().Navigation(p => p.Children).HasField(Children);
             modelBuilder.Entity<Person>().Property("name");
+            modelBuilder.Entity<Person>().Property(Name);
             modelBuilder.Entity<Person>().HasOne("father");
-            modelBuilder.Entity<Person>().HasMany("_friends");
+            modelBuilder.Entity<Person>().HasOne(Father);
+            modelBuilder.Entity<Person>().HasMany("friends");
+            modelBuilder.Entity<Person>().HasMany(Friends);
         }
     }
 
     public class Configuration : IEntityTypeConfiguration<Person>
     {
+        private const string Children = "_childrens";
+        private const string Name = "name";
+        private const string Father = "father";
+        private const string Friends = "_friends";
+
         public void Configure(EntityTypeBuilder<Person> builder)
         {
             builder.Navigation(p => p.Children).HasField("_childrens");
+            builder.Navigation(p => p.Children).HasField(Children);
             builder.Property("name");
+            builder.Property(Name);
             builder.HasOne("father");
+            builder.HasOne(Father);
             builder.HasMany("_friends");
+            builder.HasMany(Friends);
         }
     }
 }
