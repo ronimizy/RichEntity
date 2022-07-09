95ecaee7def5fff298717eee56eb91feb49a3dab
ronimizy <ronimizy@niuitmo.ru>
2022-07-09 20:00:55 +0300

    refactor: moved syntax receivers to their own folders

--- RichEntity/Generation/Entity/SyntaxReceivers/DerivedEntityInterfaceSyntaxReceiver.cs
+++ RichEntity/Generation/Entity/SyntaxReceivers/DerivedEntityInterfaceSyntaxReceiver.cs
@@ -2,9 +2,9 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RichEntity.Utility;
 
-namespace RichEntity.Generation.Entity;
+namespace RichEntity.Generation.Entity.SyntaxReceivers;
 
-public class EntityInterfaceSyntaxReceiver : ISyntaxReceiver
+public class DerivedEntityInterfaceSyntaxReceiver : ISyntaxReceiver
 {
     private readonly List<TypeDeclarationSyntax> _nodes = new List<TypeDeclarationSyntax>();
 
@@ -15,12 +15,12 @@ public class EntityInterfaceSyntaxReceiver : ISyntaxReceiver
         if (syntaxNode is not TypeDeclarationSyntax { BaseList: not null } declarationSyntax)
             return;
 
-        IEnumerable<GenericNameSyntax> interfaces = declarationSyntax.BaseList.Types
+        IEnumerable<GenericNameSyntax> directInterfaces = declarationSyntax.BaseList.Types
             .Select(t => t.Type)
             .OfType<GenericNameSyntax>()
             .Where(t => Constants.EntityInterfaceName.Equals(t.Identifier.ToString()));
 
-        if (interfaces.Any())
+        if (!directInterfaces.Any())
             _nodes.Add(declarationSyntax);
     }
 }
--- RichEntity/Generation/Entity/SyntaxReceivers/EntityInterfaceSyntaxReceiver.cs
+++ RichEntity/Generation/Entity/SyntaxReceivers/EntityInterfaceSyntaxReceiver.cs
@@ -2,9 +2,9 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RichEntity.Utility;
 
-namespace RichEntity.Generation.Entity;
+namespace RichEntity.Generation.Entity.SyntaxReceivers;
 
-public class DerivedEntityInterfaceSyntaxReceiver : ISyntaxReceiver
+public class EntityInterfaceSyntaxReceiver : ISyntaxReceiver
 {
     private readonly List<TypeDeclarationSyntax> _nodes = new List<TypeDeclarationSyntax>();
 
@@ -15,12 +15,12 @@ public class DerivedEntityInterfaceSyntaxReceiver : ISyntaxReceiver
         if (syntaxNode is not TypeDeclarationSyntax { BaseList: not null } declarationSyntax)
             return;
 
-        IEnumerable<GenericNameSyntax> directInterfaces = declarationSyntax.BaseList.Types
+        IEnumerable<GenericNameSyntax> interfaces = declarationSyntax.BaseList.Types
             .Select(t => t.Type)
             .OfType<GenericNameSyntax>()
             .Where(t => Constants.EntityInterfaceName.Equals(t.Identifier.ToString()));
 
-        if (!directInterfaces.Any())
+        if (interfaces.Any())
             _nodes.Add(declarationSyntax);
     }
 }
