3e7e957ffe8be068d3a44d15492ae08ed3a14811
ronimizy <ronimizy@niuitmo.ru>
2022-07-10 22:01:09 +0300

    refactor: constraints

--- RichEntity/Utility/Constants.cs
+++ RichEntity/Utility/Constants.cs
@@ -1,17 +1,24 @@
+using RichEntity.Annotations;
+
 namespace RichEntity.Utility;
 
 public static class Constants
 {
     public const string AnnotationsNamespace = "RichEntity.Annotations";
     
-    public const string EntityInterfaceName = "IEntity";
+    public const string EntityInterfaceName = nameof(IEntity);
     public const string EntityInterfaceFullyQualifiedName = $"{AnnotationsNamespace}.{EntityInterfaceName}`1";
     
-    public const string ConfigureConstructorsAttributeName = "ConfigureConstructorsAttribute";
+    public const string CompositeEntityInterfaceFullyQualifiedName = $"{AnnotationsNamespace}.{EntityInterfaceName}";
+    
+    public const string ConfigureConstructorsAttributeName = nameof(ConfigureConstructorsAttribute);
     public const string ConfigureConstructorsAttributeFullyQualifiedName = $"{AnnotationsNamespace}.{ConfigureConstructorsAttributeName}";
     
-    public const string ConfigureIdAttributeName = "ConfigureIdAttribute";
+    public const string ConfigureIdAttributeName = nameof(ConfigureIdAttribute);
     public const string ConfigureIdAttributeFullyQualifiedName = $"{AnnotationsNamespace}.{ConfigureIdAttributeName}";
+
+    public const string KeyPropertyAttributeName = nameof(KeyPropertyAttribute);
+    public const string KeyEntityAttributeFullyQualifiedName = $"{AnnotationsNamespace}.{KeyPropertyAttributeName}";
 
     public const string FilenameSuffix = "RichEntity.Generation.Entity.cs";
 }
