c5cbcdf706a683687d7e0d375504bf227484368f
ronimizy <ronimizy@niuitmo.ru>
2022-07-10 13:08:30 +0300

    refactor: identifier structure

--- RichEntity/Generation/Entity/Models/Identifier.cs
+++ RichEntity/Generation/Entity/Models/Identifier.cs
@@ -2,4 +2,4 @@ using Microsoft.CodeAnalysis;
 
 namespace RichEntity.Generation.Entity.Models;
 
-public record struct Identifier(string Name, ITypeSymbol Symbol);
+public record struct Identifier(string CapitalizedName, string LowercasedName, ITypeSymbol Type);
